022ee4209b09d3a4d40c5a60d3127c333d4add05
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 12:49:15 +0500

    Create begin07.py

---
+++ begin07.py
@@ -0,0 +1,7 @@
+# a o'zgaruvchiga 5 ni taminlang;
+# b o'zgaruvchiga 4 ni taminlang;
+# c o'zgaruvchiga 8 ni taminlang;
+# Quyidagi ifodani natijaga chop eting; (2a/b)c
+
+a, b, c = 5, 4, 8
+print((2 * a / b) * c)
