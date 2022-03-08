a3ae440f8b6d21e4c8fccd666728917aeb0ca86d
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 12:52:21 +0500

    Create begin09.py

---
+++ begin09.py
@@ -0,0 +1,9 @@
+# a o'zgaruvchiga 8 ni taminlang;
+# b o'zgaruvchiga 3 ni taminlang;
+# c o'zgaruvchiga 2 ni taminlang;
+# d o'zgaruvchiga 4 ni taminlang;
+# Quyidagi ifodani natijaga chop eting: (K = a + b - cd)
+
+a, b, c, d = 8, 3, 2, 4
+K = a + b - c * d
+print(K)
