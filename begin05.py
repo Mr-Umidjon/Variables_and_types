c669c870256e13c196d7f98d810abca7b7419063
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 12:43:32 +0500

    Create begin05.py

---
+++ begin05.py
@@ -0,0 +1,7 @@
+# a o'zgaruvchiga 7 ni taminlang;
+# b o'zgaruvchiga 3 ni taminlang;
+# c o'zgaruvchiga 5 ni taminlang;
+# a, b, c o'zgaruvchilar yig'indisini chop eting;
+
+a, b, c = 7, 3, 5
+print(a + b + c)
