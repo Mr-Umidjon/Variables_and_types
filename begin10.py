69a6b7db2ae3df05fbb6af4d7914108c9ac109cb
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 12:54:27 +0500

    Create begin10.py

---
+++ begin10.py
@@ -0,0 +1,9 @@
+# a o'zgaruvchiga 5 ni taminlang;
+# b o'zgaruvchiga 4 ni taminlang;
+# c o'zgaruvchiga 2 ni taminlang;
+# d o'zgaruvchiga 8 ni taminlang;
+# Quyidagi ifodaning natijasini chop eting: K = 2(ab - cd)
+
+a, b, c, d = 5, 4, 2, 8
+K = 2 * (a * b - c * d)
+print(K)
