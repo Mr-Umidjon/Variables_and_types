b1258b2c1ffc0fdfaa2da8bc99d19d457db24f8e
Umidbek <85737786+Mr-Umidjon@users.noreply.github.com>
2022-03-08 12:50:38 +0500

    Create begin08.py

---
+++ begin08.py
@@ -0,0 +1,7 @@
+# a o'zgaruvchiga 5 ni taminlang;
+# b o'zgaruvchiga 4 ni taminlang;
+# c o'zgaruvchiga 8 ni taminlang;
+# Quyidagi ifodani natijaga chop eting: ab / c
+
+a, b, c = 5, 4, 8
+print(a * b / c)
